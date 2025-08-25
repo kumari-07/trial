37b48ab1c9491c4b0f82578d4d14ecb9ea1ba30b
samkumari <kumarisam65@gmail.com>
2025-08-25 13:25:52 +0530

    jsd

---
+++ A.java
@@ -0,0 +1,14 @@
+private class A{
+    
+      void print(){
+          System.out.println("hii");
+       }
+
+}
+class B{
+public static void main(String args[]){
+
+     A a=new A();
+     a.print();
+}
+}
